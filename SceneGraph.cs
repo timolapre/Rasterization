0a7d15c5cb6991fbfeed52f055f48be0b2ba5694
Timo Lapre <timolapre1998@gmail.com>
2018-06-01 11:17:00 +0200

    .

---
+++ SceneGraph.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template_P3
+{
+    class SceneGraph
+    {
+        public void Render(Matrix CameraMatrix)
+        {
+
+        }
+    }
+}
