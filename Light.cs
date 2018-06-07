5bdc28fb733ad9d1a68cf4ef27e2b109dfc3659f
Hoyperium <menno.berga@hotmail.com>
2018-06-07 13:49:26 +0200

    Menno maakt ook contributies
    Light class added, several minor changes

---
+++ Light.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Drawing.Drawing2D;
+using OpenTK;
+using OpenTK.Input;
+using template_P3;
+namespace template_P3 {
+    public class Light
+    {
+        public Vector3 color, position;
+        float intensity;
+        public Light(Vector3 clr, Vector3 pos, float intst)
+        {
+            color = clr;
+            position = pos;
+            intensity = intst;
+
+        }
+    }
+}
