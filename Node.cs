9b7266e3bf0740df184bacaffe079b8bc5848d09
Timo Lapre <timolapre1998@gmail.com>
2018-06-07 14:15:30 +0200

    fixed?

---
+++ Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template_P3;
+
+namespace template_P3
+{
+    public class Node
+    {
+        public Mesh mesh;
+        public List<Node> Children;
+
+        public Node(Mesh mesh, List<Node> Children = null)
+        {
+            this.mesh = mesh;
+            if (Children == null)
+                this.Children = new List<Node>();
+            else
+                this.Children = Children;
+        }
+
+        public void AddChild(Node child)
+        {
+            Children.Add(child);
+        }
+
+        public void RemoveChild(Node child)
+        {
+            Children.Remove(child);
+        }
+
+        public void RemoveChild(int childIndex)
+        {
+            Children.RemoveAt(childIndex);
+        }
+    }
+}
--- SceneGraph.cs
+++ SceneGraph.cs
@@ -12,53 +12,41 @@ namespace template_P3
 {
     public class SceneGraph
     {
-
-        public List<Mesh> MeshList = new List<Mesh>();
+        Node Scene = new Node(null);
 
         // create shaders
-        Shader shader = new Shader( "../../shaders/vs.glsl", "../../shaders/fs.glsl" );
+        Shader shader = new Shader("../../shaders/vs.glsl", "../../shaders/fs.glsl");
         // load a texture
-        Texture wood = new Texture( "../../assets/wood.jpg" );
+        Texture wood = new Texture("../../assets/wood.jpg");
 
         float a = 0;
         Stopwatch timer = new Stopwatch();
 
-<<<<<<< HEAD
         public void Init()
-		{
-			Node Child = new Node(new Mesh("../../assets/floor.obj", new Vector3(0, 0, 0), new Vector3(0, 0, 0)));
-			Node Child2 = new Node(new Mesh("../../assets/teapot.obj", new Vector3(0, -2, 0), new Vector3(.5f, 0, 0)));
-			Child.AddChild(Child2);
-			Scene.AddChild(Child);
-			Child = new Node(new Mesh("../../assets/teapot.obj", new Vector3(0, 20, 0), new Vector3(0, 0, 0)));
+        {
+            Node Child = new Node(new Mesh("../../assets/floor.obj", new Vector3(0, 0, 0), new Vector3(0, 0, 0)));
+            Node Child2 = new Node(new Mesh("../../assets/teapot.obj", new Vector3(0, -2, 0), new Vector3(.5f, 0, 0)));
+            Child.AddChild(Child2);
             Scene.AddChild(Child);
-		}
+            Child = new Node(new Mesh("../../assets/teapot.obj", new Vector3(0, 20, 0), new Vector3(0, 0, 0)));
+            Scene.AddChild(Child);
+        }
 
-=======
->>>>>>> 0032aa5cd4ddebb8910c9ddd7a12400297634737
         public void Render(Matrix4 CameraMatrix)
         {
-            foreach (Mesh item in MeshList)
-            {
-                /*// measure frame duration
-                float frameDuration = timer.ElapsedMilliseconds;
-                timer.Reset();
-                timer.Start();
-
-                Matrix4 transform = Matrix4.CreateFromAxisAngle(new Vector3(0, 1, 0), a);
-                transform *= Matrix4.CreateTranslation(0, -4, -15);
-                transform *= Matrix4.CreatePerspectiveFieldOfView(1.2f, 1.3f, .1f, 1000);
-
-                // update rotation
-                a += 0.01f;
-                if (a > 2 * Math.PI) a -= 2 * (float)Math.PI;*/
-
-                item.Render(shader, CameraMatrix, wood);
-            }
+            foreach (Node child in Scene.Children)
+                RenderChildren(child, CameraMatrix);
         }
-        public void Add(Mesh mesh)
+
+        public void RenderChildren(Node parent, Matrix4 LocalMatrix)
         {
-            MeshList.Add(mesh);
+            LocalMatrix.Row3 += new Vector4(parent.mesh.offset, parent.mesh.offset.Z);
+            Vector4 row0 = LocalMatrix.Row0;
+            //LocalMatrix = Matrix4.CreateFromAxisAngle(new Vector3(0, 1, 0), a);
+            //LocalMatrix.Row0 = new Vector4((float)Math.Cos(Math.Acos(row0.X) + parent.mesh.Rotation.X), 0, (float)Math.Sin(Math.Asin(row0.Z)+parent.mesh.Rotation.X), (float)Math.Sin(Math.Asin(row0.W)+parent.mesh.Rotation.X));
+            parent.mesh.Render(shader, LocalMatrix, wood);
+            foreach (Node child in parent.Children)
+                RenderChildren(child, LocalMatrix);
         }
     }
 }
