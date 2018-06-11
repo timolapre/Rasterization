927132cc4675b545837f685a84baddb67c30287d
Hoyperium <menno.berga@hotmail.com>
2018-06-11 14:49:12 +0200

    Light class added
    for realzies this time

---
+++ Light.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using template_P3;
+
+public class Light
+{
+    public Vector3 position;
+    public Vector3 color;
+    public float brightness;
+
+	public Light(Vector3 position, Vector3 color, float brightness)
+	{
+        this.position = position;
+        this.color = color;
+        this.brightness = brightness;
+
+	}
+}
