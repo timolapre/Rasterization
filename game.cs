899b93b08433aab45ae8152f74db33dfc76bead1
Timo Lapre <timolapre1998@gmail.com>
2018-06-15 19:29:46 +0200

    .

--- game.cs
+++ game.cs
@@ -58,7 +58,7 @@ namespace Template_P3
  
             int lightID = GL.GetUniformLocation(shader.programID,"lightPos");
             GL.UseProgram(shader.programID);
-            GL.Uniform3(lightID, 0.0f, 0.0f, 0.0f);
+            GL.Uniform3(lightID, 0.0f, 20.0f, 0.0f);
 
             sceneGraph.Init();
         }
